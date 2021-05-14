414f835c2ae22876fc23c85618c9c55be2c9886a
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-14 00:03:30 -0300

    Project created

---
+++ src/main/java/com/workshopspringboot/workshopspringboot/WorkshopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.workshopspringboot.workshopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringBootApplication.class, args);
+	}
+
+}
