2739d7e59ac7bae7128324164f538c2023e5a31e
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-18 16:25:33 -0300

    Exception handling - update

--- src/main/java/com/workshopspringboot/workshopspringboot/services/UserService.java
+++ src/main/java/com/workshopspringboot/workshopspringboot/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,9 +32,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getOne(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getOne(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
