7129fc5aa5dd58954da41eff940f4d42e592326d
kaique oliveira santos <kaiquevicio@gmail.com>
2021-05-18 16:18:54 -0300

    Exception handling - delete

--- src/main/java/com/workshopspringboot/workshopspringboot/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/workshopspringboot/workshopspringboot/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,8 @@
-package com.workshopspringboot.workshopspringboot.services.exceptions;
+package com.workshopspringboot.workshopspringboot.resources.exceptions;
 
 import com.workshopspringboot.workshopspringboot.resources.exceptions.StandardError;
+import com.workshopspringboot.workshopspringboot.services.exceptions.DatabaseException;
+import com.workshopspringboot.workshopspringboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +18,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
--- src/main/java/com/workshopspringboot/workshopspringboot/services/UserService.java
+++ src/main/java/com/workshopspringboot/workshopspringboot/services/UserService.java
@@ -2,8 +2,11 @@ package com.workshopspringboot.workshopspringboot.services;
 
 import com.workshopspringboot.workshopspringboot.entities.User;
 import com.workshopspringboot.workshopspringboot.repositories.UserRepository;
+import com.workshopspringboot.workshopspringboot.services.exceptions.DatabaseException;
 import com.workshopspringboot.workshopspringboot.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -40,6 +43,12 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 }
